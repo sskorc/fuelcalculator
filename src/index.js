8b7bfd7b9de18aebfed97d158e68c0108756981f
Szymon Skórczyński <szymon.skorczynski@schibsted.pl>
2020-05-23 17:44:49 +0200

    refactoring done

--- src/index.js
+++ src/index.js
@@ -1,67 +1,3 @@
-function calculateRaceLaps(raceTime, lapTime) {
-    return Math.ceil(raceTime / lapTime);
-}
-
-function calculateFuelConsumptionPerLap(fuel, laps) {
-    return fuel / laps;
-}
-
-function calculateFuelNeed(fuelConsumptionPerLap, laps) {
-    return Math.ceil(fuelConsumptionPerLap * laps);
-}
-
-function manageRaceTime(raceTimeHours, raceTimeMinutes, lapTimeMinutes, lapTimeSeconds) {
-    if (isNumber(raceTimeHours) && isNumber(raceTimeMinutes) && isNumber(lapTimeMinutes) && isNumber(lapTimeSeconds)) {
-        raceTime = (Math.abs(Number(raceTimeHours)) * 60 + Math.abs(Number(raceTimeMinutes))) * 60;
-        lapTime = Math.abs(Number(lapTimeMinutes)) * 60 + Math.abs(Number(lapTimeSeconds));
-
-        raceLaps = calculateRaceLaps(raceTime, lapTime);
-        printResult('raceTime', raceLaps);
-    } else {
-        raceTime = 0;
-        lapTime = 0
-        raceLaps = 0;
-
-        clearResult('raceTime');
-    }
-}
-
-function manageFuelConsumption(fuelConsumptionLiters, fuelConsumptionLaps) {
-    if (isNumber(fuelConsumptionLiters) && Number(fuelConsumptionLiters) !== 0 && isNumber(fuelConsumptionLaps) && Number(fuelConsumptionLaps) !== 0) {
-        fuelConsumptionPerLap = calculateFuelConsumptionPerLap(Math.abs(Number(fuelConsumptionLiters)), Math.abs(Number(fuelConsumptionLaps)));
-
-        printResult('fuelConsumption', fuelConsumptionPerLap);
-    } else {
-        fuelConsumptionPerLap = 0;
-
-        clearResult('fuelConsumption');
-    }
-}
-
-function manageAdditionalFuel(additionType, isSelected, additionalValue) {
-    if(isSelected && raceLaps > 0 && fuelConsumptionPerLap > 0) {                
-        if (isNumber(additionalValue)) {
-            switch(additionType) {
-                case 'additionalLaps':
-                    additionalFuelNeed = calculateFuelNeed(fuelConsumptionPerLap, Math.abs(Number(additionalValue)));
-                    break;
-                case 'additionalLiters':
-                    additionalFuelNeed = Math.abs(Number(additionalValue));
-                    break;
-                case 'additionalPercentage':
-                    additionalFuelNeed = Math.ceil(Math.abs(Number(additionalValue))/100 * fuelNeed);
-                    break;
-                default:
-                    additionalFuelNeed = 0;    
-            }            
-        } else {
-            additionalFuelNeed = 0;
-        }               
-    } else {
-        fuelNeed = 0;
-    }
-}
-
 function isNumber(number) {
     return number !== '' && !isNaN(number);
 }
@@ -93,154 +29,184 @@ function clearResult(section) {
 function calculate() {
     const values = getInputValues();
 
-    manageRaceTime(values.raceTimeHours, values.raceTimeMinutes, values.lapTimeMinutes, values.lapTimeSeconds);
+    raceLaps.manage(values.raceTimeHours, values.raceTimeMinutes, values.lapTimeMinutes, values.lapTimeSeconds);
+    fuelConsumption.manage(values.fuelConsumptionLiters, values.fuelConsumptionLaps);
+    additionalFuel.getNumericFields().forEach((field) => {
+        if (values[field + 'Selected']) { additionalFuel.manage(field, true, values[field]); }
+    })
 
-    manageFuelConsumption(values.fuelConsumptionLiters, values.fuelConsumptionLaps);
-
-    if (values.isAdditionalLapsSelected) {
-        manageAdditionalFuel('additionalLaps', true, values.additionalLaps);
-    }
-
-    if (values.isAdditionalLitersSelected) {
-        manageAdditionalFuel('additionalLiters', true, values.additionalLiters);
-    }
-
-    if (values.isAdditionalPercentageSelected) {     
-        manageAdditionalFuel('additionalPercentage', true, values.additionalPercentage/100);
-    }
-
-    if (raceLaps > 0 && fuelConsumptionPerLap > 0) {
-        fuelNeed = calculateFuelNeed(fuelConsumptionPerLap, raceLaps) + additionalFuelNeed;
-        printResult('final', fuelNeed)
-    } else {
-        clearResult('final');
-    }
+    finalFuel.manage(raceLaps.result, fuelConsumption.result, additionalFuel.result);
 }
 
-function getInputValues() {
-    return {
-        raceTimeHours: Number(document.getElementById("raceTimeHours").value),
-        raceTimeMinutes: Number(document.getElementById("raceTimeMinutes").value),
-        lapTimeMinutes: Number(document.getElementById("lapTimeMinutes").value),
-        lapTimeSeconds: Number(document.getElementById("lapTimeSeconds").value),
-        fuelConsumptionLiters: Number(document.getElementById("fuelConsumptionLiters").value),
-        fuelConsumptionLaps: Number(document.getElementById("fuelConsumptionLaps").value),
-        additionalLaps: Number(document.getElementById("additionalLaps").value),
-        additionalLiters: Number(document.getElementById("additionalLiters").value),
-        additionalPercentage: Number(document.getElementById("additionalPercentage").value),
-        isAdditionalLapsSelected: document.getElementById("additionalLapsSelected").checked,
-        isAdditionalLitersSelected: document.getElementById("additionalLitersSelected").checked,
-        isAdditionalPercentageSelected: document.getElementById("additionalPercentageSelected").checked,
-        toCSV: function () {
-            return `${this.raceTimeHours},${this.raceTimeMinutes},${this.lapTimeMinutes},${this.lapTimeSeconds},${this.fuelConsumptionLiters},${this.fuelConsumptionLaps},${this.additionalLaps},${this.additionalLiters},${this.additionalPercentage},${this.isAdditionalLapsSelected},${this.isAdditionalLitersSelected},${this.isAdditionalPercentageSelected}`;
-        }
-    };
-}
-
-function getInputValuesFromCSV(csv) {
-    const values = csv.split(',');
-
-    return {
-        raceTimeHours: Number(values[0]),
-        raceTimeMinutes: Number(values[1]),
-        lapTimeMinutes: Number(values[2]),
-        lapTimeSeconds: Number(values[3]),
-        fuelConsumptionLiters: Number(values[4]),
-        fuelConsumptionLaps: Number(values[5]),
-        additionalLaps: Number(values[6]),
-        additionalLiters: Number(values[7]),
-        additionalPercentage: Number(values[8]),
-        isAdditionalLapsSelected: (values[9] === 'true'),
-        isAdditionalLitersSelected: (values[10] === 'true'),
-        isAdditionalPercentageSelected: (values[11] === 'true'),
-    }
-}
-
-function setInputValues(values) {
-    document.getElementById("raceTimeHours").value = values.raceTimeHours;
-    document.getElementById("raceTimeMinutes").value = values.raceTimeMinutes;
-    document.getElementById("lapTimeMinutes").value = values.lapTimeMinutes;
-    document.getElementById("lapTimeSeconds").value = values.lapTimeSeconds;
-    document.getElementById("fuelConsumptionLiters").value = values.fuelConsumptionLiters;
-    document.getElementById("fuelConsumptionLaps").value = values.fuelConsumptionLaps;
-    document.getElementById("additionalLaps").value = values.additionalLaps;
-    document.getElementById("additionalLiters").value = values.additionalLiters;
-    document.getElementById("additionalPercentage").value = values.additionalPercentage;
-    document.getElementById("additionalLapsSelected").checked = values.isAdditionalLapsSelected;
-    document.getElementById("additionalLitersSelected").checked = values.isAdditionalLitersSelected;
-    document.getElementById("additionalPercentageSelected").checked = values.isAdditionalPercentageSelected;
-}
-
-
-
-function calculate2(input, inputs) {
-    if (input.id === "raceTimeHours" || input.id === "raceTimeMinutes" || input.id === "lapTimeMinutes" || input.id === "lapTimeSeconds") {
-        const raceTimeHours = inputs.namedItem('raceTimeHours').value;
-        const raceTimeMinutes = inputs.namedItem('raceTimeMinutes').value;
-        const lapTimeMinutes = inputs.namedItem('lapTimeMinutes').value;
-        const lapTimeSeconds = inputs.namedItem('lapTimeSeconds').value;
-
-        manageRaceTime(raceTimeHours, raceTimeMinutes, lapTimeMinutes, lapTimeSeconds);
-    } else if (input.id === "fuelConsumptionLiters" || input.id === "fuelConsumptionLaps") {
-        const fuelConsumptionLiters = inputs.namedItem("fuelConsumptionLiters").value;
-        const fuelConsumptionLaps = inputs.namedItem("fuelConsumptionLaps").value;
-
-        manageFuelConsumption(fuelConsumptionLiters, fuelConsumptionLaps);
-    } else if (["additionalLaps", "additionalLiters", "additionalPercentage"].includes(input.id)) {
-        const isAdditionalFieldSelected = inputs.namedItem(input.id + "Selected").checked;
-        manageAdditionalFuel(input.id, isAdditionalFieldSelected, input.value);
-    } else if (["additionalLapsSelected", "additionalLitersSelected", "additionalPercentageSelected"].includes(input.id)) {
-        const foo = input.id.replace("Selected", "");
-        const additionalValue = inputs.namedItem(foo).value;
-        manageAdditionalFuel(foo, input.checked, additionalValue);
+function observeField(input, inputs) {
+    if (raceLaps.getFields().includes(input.id)) {
+        values = raceLaps.getFields().map((fieldName) => {
+            return inputs.namedItem(fieldName).value;
+        });
+        raceLaps.manage(...values);
+    } else if (fuelConsumption.getFields().includes(input.id)) {
+        values = fuelConsumption.getFields().map((fieldName) => {
+            return inputs.namedItem(fieldName).value;
+        });
+        fuelConsumption.manage(...values);
+    } else if (additionalFuel.getNumericFields().includes(input.id)) {
+        additionalFuel.manage(input.id, inputs.namedItem(input.id + "Selected").checked, input.value);
+    } else if (additionalFuel.getBooleanFields().includes(input.id)) {
+        const additionalFuelField = input.id.replace("Selected", "");
+        const additionalValue = inputs.namedItem(additionalFuelField).value;
+        additionalFuel.manage(additionalFuelField, input.checked, additionalValue);
     } else if (input.id === "nothingAdditionalSelected") {
         if (input.checked) {
-            additionalFuelNeed = 0;
+            additionalFuel.clear();
         }
     }
 
-    if(additionalFuelNeed == 0 && input.id !== "nothingAdditionalSelected") {
-        ["additionalLaps", "additionalLiters", "additionalPercentage"].forEach((item) => {
+    if(additionalFuel.result == 0 && input.id !== "nothingAdditionalSelected") {
+        additionalFuel.getNumericFields().forEach((item) => {
             if (inputs.namedItem(item + "Selected").checked) {
-                manageAdditionalFuel(item, true, inputs.namedItem(item).value);
+                additionalFuel.manage(item, true, inputs.namedItem(item).value);
             }            
         });
     }
 
-    if (raceLaps > 0 && fuelConsumptionPerLap > 0) {
-        fuelNeed = calculateFuelNeed(fuelConsumptionPerLap, raceLaps) + additionalFuelNeed;
-        printResult('final', fuelNeed)
-    } else {
-        clearResult('final');
+    finalFuel.manage(raceLaps.result, fuelConsumption.result, additionalFuel.result);
+}
+
+function getInputValues() {
+    const numberValues = numericFields.reduce((acc, cur) => {
+        acc[cur] = Number(document.getElementById(cur).value);
+        return acc;
+    }, {});
+
+    const booleanValues = booleanFields.reduce((acc, cur) => {
+        acc[cur] = document.getElementById(cur).checked;
+        return acc;
+    }, {});
+
+    return Object.assign(numberValues, booleanValues);
+}
+
+function exportValuesToCSV(values) {
+    return Object.values(values).reduce(function(acc, cur, i) {
+        return acc + (i !== 0 ? ',' : '') + cur;
+      }, '');
+}
+
+function importValuesFromCSV(csv) {
+    const values = csv.split(',');
+
+    numericFields.forEach((fieldName, i) => {
+        document.getElementById(fieldName).value = Number(values[i]);
+    });
+
+    booleanFields.forEach((fieldName, i) => {
+        document.getElementById(fieldName).checked = (values[i + 9] === 'true');
+    });
+}
+
+const raceLaps = {
+    result: 0,
+    getFields() { return ["raceTimeHours", "raceTimeMinutes", "lapTimeMinutes", "lapTimeSeconds"] },
+    manage(raceTimeHours, raceTimeMinutes, lapTimeMinutes, lapTimeSeconds) {
+        if (isNumber(raceTimeHours) && isNumber(raceTimeMinutes) && isNumber(lapTimeMinutes) && isNumber(lapTimeSeconds)) {
+            raceTime = (Math.abs(Number(raceTimeHours)) * 60 + Math.abs(Number(raceTimeMinutes))) * 60;
+            lapTime = Math.abs(Number(lapTimeMinutes)) * 60 + Math.abs(Number(lapTimeSeconds));
+    
+            this.result = this.calculate(raceTime, lapTime);
+            printResult('raceTime', this.result);
+        } else {
+            this.result = 0;    
+            clearResult('raceTime');
+        }
+    },
+    calculate(raceTime, lapTime) {
+        return Math.ceil(raceTime / lapTime)
+    }
+};
+
+const fuelConsumption = {
+    result: 0,
+    getFields() { return ["fuelConsumptionLiters", "fuelConsumptionLaps"] },
+    manage(fuelConsumptionLiters, fuelConsumptionLaps) {
+        if (isNumber(fuelConsumptionLiters) && Number(fuelConsumptionLiters) !== 0 && isNumber(fuelConsumptionLaps) && Number(fuelConsumptionLaps) !== 0) {
+            this.result = this.calculate(Math.abs(Number(fuelConsumptionLiters)), Math.abs(Number(fuelConsumptionLaps)));
+    
+            printResult('fuelConsumption', this.result);
+        } else {
+            this.result = 0;
+    
+            clearResult('fuelConsumption');
+        }
+    },
+    calculate(fuel, laps) {
+        return fuel / laps;
     }
 }
 
-var raceTime = 0;
-var lapTime = 0;
-var raceLaps = 0;
-var fuelConsumptionPerLap = 0;
-var additionalFuelNeed = 0;
-var fuelNeed = 0;
+const additionalFuel = {
+    result: 0,
+    getNumericFields() { return ["additionalLaps", "additionalLiters", "additionalPercentage"] },
+    getBooleanFields() { return ["additionalLapsSelected", "additionalLitersSelected", "additionalPercentageSelected"] },
+    manage(additionType, isSelected, additionalValue) {
+        if(isSelected && raceLaps.result > 0 && fuelConsumption.result > 0) {                
+            if (isNumber(additionalValue)) {
+                switch(additionType) {
+                    case 'additionalLaps':
+                        this.result = finalFuel.calculate(fuelConsumption.result, Math.abs(Number(additionalValue)));
+                        break;
+                    case 'additionalLiters':
+                        this.result = Math.abs(Number(additionalValue));
+                        break;
+                    case 'additionalPercentage':
+                        this.result = Math.ceil(Math.abs(Number(additionalValue))/100 * finalFuel.result);
+                        break;
+                    default:
+                        this.result = 0;    
+                }            
+            } else {
+                this.result = 0;
+            }               
+        } else {
+            finalFuel.clear();
+        }
+    },
+    clear() { this.result = 0 }
+}
+
+const finalFuel = {
+    result: 0,
+    manage(raceLapsResult, fuelConsumptionResult, additionalFuelResult) {
+        if (raceLapsResult > 0 && fuelConsumptionResult > 0) {
+            this.result = this.calculate(fuelConsumptionResult, raceLapsResult) + additionalFuelResult;
+            printResult('final', this.result)
+        } else {
+            clearResult('final');
+        }
+    },
+    calculate(fuelConsumptionPerLap, laps) {
+        return Math.ceil(fuelConsumptionPerLap * laps);
+    },
+    clear() { this.result = 0; }
+}
+
+const numericFields = [].concat(raceLaps.getFields(), fuelConsumption.getFields(), additionalFuel.getNumericFields());
+const booleanFields = additionalFuel.getBooleanFields();
 
 const url = new URL(document.URL);
-const sh = url.searchParams.get("sh");
-if(sh) {
-    setInputValues(getInputValuesFromCSV(atob(sh)));
+const importString = url.searchParams.get("sh");
+if(importString) {
+    importValuesFromCSV(atob(importString));
     calculate();
 }
 
 const inputs = document.getElementsByTagName("input");
-
-console.log(inputs.namedItem('raceTimeHours'));
-
 for (let input of inputs) {
     input.addEventListener("input", () => {
-        calculate2(input, inputs);
+        observeField(input, inputs);
     });
-
 }
 
 document.getElementById("shareButton").addEventListener("click", () => {
-    const encodedString = btoa(getInputValues().toCSV());
+    const encodedString = btoa(exportValuesToCSV(getInputValues()));
     console.log(location.protocol + '//' + location.host + location.pathname + '?sh=' + encodedString);
 });
